39828b4239080a05a046bc1cf70d0483f563bbd5
w3-80717 <chavan1609@gmail.com>
2024-09-11 12:56:28 +0530

    background image removed

--- frontend1/src/pages/Home.js
+++ frontend1/src/pages/Home.js
@@ -7,19 +7,19 @@ const projects = [
     id: 1,
     title: "Project One",
     description: "This is a brief description of project one.",
-    imageUrl: "",
+    imageUrl: "/images/mango1.jpeg",
   },
   {
     id: 2,
     title: "Project Two",
     description: "This is a brief description of project two.",
-    imageUrl: "./public/images/mangob.jpeg",
+    imageUrl: "/images/mango1.jpeg",
   },
   {
     id: 3,
     title: "Project Three",
     description: "This is a brief description of project three.",
-    imageUrl: "https://via.placeholder.com/300x200",
+    imageUrl: "/images/mango1.jpeg",
   },
 ];
 
@@ -29,7 +29,7 @@ const Home = () => {
       {/* Hero Section */}
       <section className="hero-section">
         <img
-          src="https://via.placeholder.com/1200x400"
+          src="/images/rmimango.jpg"
           alt="Hero"
           className="hero-image"
         />
